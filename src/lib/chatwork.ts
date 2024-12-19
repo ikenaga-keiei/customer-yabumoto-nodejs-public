904b56df4bda38fe521fb69b982fd2b7ed7e65b4
ikenaga-keiei <ikenaga.sysmainte@gmail.com>
2024-12-19 16:48:59 +0900

    🏷️ チャットワークメッセージ送信先のルームIDを変更

--- src/lib/chatwork.ts
+++ src/lib/chatwork.ts
@@ -5,7 +5,7 @@ config();
 const chatworkClient = new ChatworkApi(process.env.CHATWORK_API_TOKEN);
 
 export const sendMessageForIkenagaChatwork = (message: string) => {
-  return chatworkClient.postRoomMessage(238818241, {
+  return chatworkClient.postRoomMessage(373789581, {
     body: `[info][title]${process.env.KINTONE_BASE_URL}でのエラー[/title]${message}[/info]`,
   });
 };
